03f98117b38e59efe0aa8dc3b181b2ed61fe27d5
Tom Coleman <tom@thesnail.org>
2017-05-31 11:01:08 +1000

    Failed to repro #877

--- examples/test-cra/src/stories/index.js
+++ examples/test-cra/src/stories/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import Button from './Button';
 import Welcome from './Welcome';
 import ComponentWithRef from './ComponentWithRef';
+
+addDecorator(s => <div class="root">{s()}</div>);
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
